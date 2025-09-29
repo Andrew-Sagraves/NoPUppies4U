79552b84889f6cdb7f5c8d7956ef6c4f4ddc0430
hclark37 <hclark37@vols.utk.edu>
2025-09-29 19:36:06 -0400

    Update main.cpp
    Added Kaden's and Bryan's work to main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include <string>
 //#include "main.hpp"
 //#include "asagrave.h"
 #include <getopt.h> 
 #include "asagrave.h"
 #include "bmulli21.h"
 #include "jdong11.h"
+#include "kbissonn.h"
 #include <vector>
 #include <iomanip>
 
@@ -28,12 +30,11 @@ int main(int argc, char* argv[]) {
 	
 	static struct option long_options[] = {
 		{"help", no_argument,		0, 'h'},
-		{"verbose", no_argument,	0, 'v'},
 		{"crontab", no_argument,	0, 'c'},
-		{"sudo", no_argument,		0, 's'},
 		{"path", no_argument,		0, 'p'},
 		{"sources", no_argument,	0, 'x'},
 		{"all", no_argument,		0, 'a'},
+		{"directory", required_argument,	0, 'd'},
 		{0, 0, 0, 0}
 	};
 	
@@ -45,7 +46,7 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 	
-	while ((opt = getopt_long(argc, argv, "hvscpxa", long_options, &options_index)) != -1) { //hvscp lets short options work, like -s
+	while ((opt = getopt_long(argc, argv, "hcpxad:", long_options, &options_index)) != -1) { //hvscp lets short options work, like -s
 		//you have to make sure to add any additional options to that ""
 		
 		switch (opt) {
@@ -54,12 +55,11 @@ int main(int argc, char* argv[]) {
 				cout << "Options:" << endl;
 
 				cout << "  " << left << setw(25) << "-h,   --help"    << "Show this help message" << endl;
-				cout << "  " << left << setw(25) << "-v,   --verbose" << "Enable verbose output" << endl;
 				cout << "  " << left << setw(25) << "-c,   --crontab" << "Check crontab" << endl;
-				cout << "  " << left << setw(25) << "-s,   --sudo"    << "Check sudo permissions" << endl;
 				cout << "  " << left << setw(25) << "-x,   --sources" << "Check all sources" << endl;
 				cout << "  " << left << setw(25) << "-p,   --path"    << "Check path" << endl;
 				cout << "  " << left << setw(25) << "-a,   --all"     << "Run all tests" << endl;
+				cout << "  " << left << setw(25) << "-d,   --directory"     << "Check directory for changes" << endl;
 				return 0;
 				break;
 				
@@ -72,23 +72,28 @@ int main(int argc, char* argv[]) {
 				int problems = get_path_vulnerabilities(paths);
 				cout << "PATH scan complete. " << problems << " potential issue(s) found. Issues outputted to PATH.txt\n";
 			
+				check_cron_jobs();
+				
+				//i'm choosing to not include directory case in this because it would require the --all flag to take an argument, which wouldn't really work if another one required an argument as well
+				
 				return 0;
 				break;
 			}
 			case 'x':
 				check_sources_list();
 				break;
-			case 'v':
-				
+			
+			case 'd': {
+				if (optarg == nullptr) {
+					cerr << "Error: --directory requires an argument" << endl;
+					return 1;
+				}
+				string directory = optarg;
+				check_directory_for_changes(directory);
 				break;
-			case 'o':
-				
-				break;
-			case 's':
-				
-				break;
+			}
 			case 'c':
-				
+				check_cron_jobs();
 				break;
 			case 'p': {
 				vector<string> paths = get_paths();
