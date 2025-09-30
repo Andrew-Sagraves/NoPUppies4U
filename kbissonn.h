5aa7ee77c25ecdbbac1db0c73dcc97a0403b5041
Kaden-Bissonnette <165855329+Kaden-Bissonnette@users.noreply.github.com>
2025-09-29 22:01:10 -0400

    Adding more comments to kbissonn.h

--- kbissonn.h
+++ kbissonn.h
@@ -10,15 +10,21 @@ using namespace std;
 
 
 //helper functions for check_cron_jobs()
+// Go through and see if it is a schedule token (5 * * * *, @reboot, etc)
 bool is_schedule_token(const string &t);
+// Split a string into tokens, preserving quoted substrings (mainly to get the path name to pass to extract path)
 vector<string> split_tokens(const string &s);
+// Removing extra quotes and special chars
 string sanitize_token(string t);
+// Get everthring that looks like a path from a command string
 vector<string> extract_paths(const string &cmd);
+// Get everthring that looks like a path from a command string
 void process_file(const filesystem::path &p, set<string> &checked_files);
 
+// Checks for cron jobs in /etc/cron.d, /etc/cron.daily, /etc/cron.hourly, /etc/cron.monthly, /etc/cron.weekly
 void check_cron_jobs();
 
-// For verbose
+// For verbose (prints all function calls from all of the cron jobs to console)
 void list_cronjob_commands();
 
 
