0c11e5428be543aa8c1ee949b493cc10d5579a26
hclark37 <hclark37@vols.utk.edu>
2025-10-12 13:11:50 -0400

    Create hclark37.h
    added header for my previous function

---
+++ hclark37.h
@@ -0,0 +1,14 @@
+#ifndef HCLARK37_H
+#define HCLARK37_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void check_empty_passwords();
+
+#endif
